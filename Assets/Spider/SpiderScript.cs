829efb5b20d165453ec8b454d2ab663f6f5a2dc6
timoffex <timoffex@gmail.com>
2016-08-17 00:08:10 -0700

    Added a spider

---
+++ Assets/Spider/SpiderScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpiderScript : MonoBehaviour {
+
+
+	private Animator controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		controller.SetBool ("Moving", true);
+
+		var player = NearestPlayer ();
+
+		var towardPlayer = (player.position - transform.position).normalized;
+
+		transform.up = -towardPlayer;
+		transform.position += towardPlayer*0.1f;
+	}
+
+
+	Transform NearestPlayer () {
+		var players = GameObject.FindGameObjectsWithTag ("Player");
+
+		var minDist = Mathf.Infinity;
+		GameObject closest = null;
+
+		foreach (GameObject p in players) {
+			var dist = (p.transform.position - transform.position).sqrMagnitude;
+
+			if (dist < minDist) {
+				minDist = dist;
+				closest = p;
+			}
+		}
+
+		return closest.transform;
+	}
+}
