24ec9110e9b47980ada238d6e5d351e74ee53b67
timoffex <timoffex@gmail.com>
2016-08-17 01:03:52 -0700

    more spider ai

--- Assets/Spider/SpiderScript.cs
+++ Assets/Spider/SpiderScript.cs
@@ -1,26 +1,77 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Animator))]
 public class SpiderScript : MonoBehaviour {
+	
+	 private Animator controller;
 
 
-	private Animator controller;
 
-	// Use this for initialization
+	public float restDuration = 1;
+	public float chaseDuration = 3;
+
+	private float lastRecordedTime = 0;
+
+
+	private enum AIState { Chase, Rest };
+	private AIState aiState = AIState.Rest;
+	private Vector3 targetPosition;
+
+
 	void Start () {
 		controller = GetComponent<Animator> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		controller.SetBool ("Moving", true);
+		PerformAI ();
+	}
+
+
+
+	void PerformAI () {
 
 		var player = NearestPlayer ();
 
-		var towardPlayer = (player.position - transform.position).normalized;
+		if (aiState == AIState.Rest) {
+			controller.SetBool ("Moving", false);
 
-		transform.up = -towardPlayer;
-		transform.position += towardPlayer*0.1f;
+			if (Time.time - lastRecordedTime > restDuration) {
+				lastRecordedTime = Time.time;
+				aiState = AIState.Chase;
+
+
+				var noise = Random.insideUnitSphere;
+				noise.z = 0;
+				targetPosition = player.position + 10*noise;
+			}
+		} else {
+			controller.SetBool ("Moving", true);
+
+			var toTarget = targetPosition - transform.position;
+			var dist = toTarget.magnitude;
+			toTarget /= dist;
+
+
+			FaceDirection (toTarget);
+
+			var speed = 0.05f;
+
+			if (dist > speed) {
+				transform.position += toTarget * speed;
+			}
+
+
+			if (dist < speed || Time.time - lastRecordedTime > chaseDuration) {
+				lastRecordedTime = Time.time;
+				aiState = AIState.Rest;
+			}
+		}
+	}
+
+	void FaceDirection (Vector3 dir) {
+		transform.up = -dir;
 	}
 
 
