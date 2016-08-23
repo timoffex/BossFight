dd243e610e225d96a7519102b99ab10d0a4a89c8
Reshma Z <nomnomreshma@gmail.com>
2016-08-22 18:23:17 -0700

    Projectiles do damage!!!!!
    and we can kill spiders!!

---
+++ Assets/Scripts/Combat.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Combat : MonoBehaviour {
+	public float health = 100.0f;
+
+	public void TakeDamage (float dmg) {
+		health -= dmg;
+		print (health);
+		// display damage taken
+		if (health <= 0.0) {
+			//display death animation
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/ProjectileTypes.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileTypes : MonoBehaviour {
+	public float damage = 0.0f;
+
+	void OnCollisionEnter2D (Collision2D col){
+		print (col.gameObject.name);
+		var combat = col.gameObject.GetComponent<Combat>();
+		if (combat == null) {
+			print ("Combat.cs is null");
+		} else {
+			combat.TakeDamage (damage);
+			Destroy (gameObject);
+		}
+	}
+}
