1a59b5ae1b42fe2eaccc7f9be52cd997298cf5b0
timoffex <timoffex@gmail.com>
2016-08-22 18:27:14 -0700

    added damage to projectile

---
+++ Assets/Scripts/BoulderAI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoulderAI : MonoBehaviour {
+
+
+	private BoulderActions actions;
+
+	// Use this for initialization
+	void Start () {
+		actions = GetComponent<BoulderActions> ();
+		StartCoroutine ("LateStart");
+	}
+
+	IEnumerator LateStart () {
+		yield return new WaitForEndOfFrame ();
+		StartCoroutine ("DecisionLoop");
+	}
+
+	IEnumerator DecisionLoop () {
+		while (true) {
+			// Do nothing.
+			yield return new WaitForFixedUpdate ();
+		}
+	}
+}
